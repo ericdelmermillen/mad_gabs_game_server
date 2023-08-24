f7b4379fa84b8ced4ed3f84ba07b9ee7160c8071
Eric <ericdelmermillen@gmail.com>
2023-08-24 08:48:55 -0400

    refactored gabs to each be an array of 2 arrays

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.use(express.json());
 // Routes:
 app.use('/', routes);
 
-const port = process.env.PORT || 8088;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Listening on ${port}`));
