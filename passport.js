a29cc827b65d8bb5cd4c32c0946fc5a9b0530698
Eric Delmer Millen <ericdelmermillen@gmail.com>
2023-09-17 00:18:27 -0400

    added full callback url in passport.js to troubleshoot callback mismatch

--- passport.js
+++ passport.js
@@ -8,13 +8,15 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      // callbackURL: "/auth/google/callback",
+      callbackURL: "https://mad-gabs-game-server-a3fe555ec3c0.herokuapp.com/auth/google/callback",
+
     },
     function (accessToken, refreshToken, profile, done) {
       done(null, profile);
     }
-    )
-    );
+  )
+);
     
 
 // FACEBOOK_APP_ID = "your id";
